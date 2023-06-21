aa72a04f69ea2547f9d7c0875e91177e58acf40b
Yuhei <yuhei@X1C6-MAC.local>
2023-06-21 16:26:59 +1200

    Refactor test code

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -4,25 +4,25 @@ import { Appointment } from "../src/Appointment";
 import { act } from "react-dom/test-utils";
 
 describe("Appointment", () => {
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
+
+  const render = (component) =>
+    act(() => ReactDOM.createRoot(container).render(component));
+
   it("renders the customer first name", () => {
     const customer = { firstName: "Ashley" };
-    const component = <Appointment customer={customer} />;
-
-    const container = document.createElement("div");
-    document.body.replaceChildren(container);
-
-    act(() => ReactDOM.createRoot(container).render(component));
+    render(<Appointment customer={customer} />);
     expect(document.body.textContent).toContain("Ashley");
   });
 
   it("renders another customer first name", () => {
     const customer = { firstName: "Jordan" };
-    const component = <Appointment customer={customer} />;
-
-    const container = document.createElement("div");
-    document.body.replaceChildren(container);
-
-    act(() => ReactDOM.createRoot(container).render(component));
+    render(<Appointment customer={customer} />);
     expect(document.body.textContent).toContain("Jordan");
   });
 });
