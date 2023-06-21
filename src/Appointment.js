e45b36e1dd6dcd740f6e90750f5fdde41c4db76f
Yuhei <yuhei@X1C6-MAC.local>
2023-06-21 16:04:29 +1200

    Implement prop within Appointment component

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,1 +1,3 @@
-export const Appointment = () => "Ashley";
+import React from "react";
+
+export const Appointment = ({ customer }) => <div>{customer.firstName}</div>;
--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -5,13 +5,24 @@ import { act } from "react-dom/test-utils";
 
 describe("Appointment", () => {
   it("renders the customer first name", () => {
-    const customer = { firstName: "Ashely" };
+    const customer = { firstName: "Ashley" };
     const component = <Appointment customer={customer} />;
 
     const container = document.createElement("div");
-    document.body.appendChild(container);
+    document.body.replaceChildren(container);
 
     act(() => ReactDOM.createRoot(container).render(component));
     expect(document.body.textContent).toContain("Ashley");
+  });
+
+  it("renders another customer first name", () => {
+    const customer = { firstName: "Jordan" };
+    const component = <Appointment customer={customer} />;
+
+    const container = document.createElement("div");
+    document.body.replaceChildren(container);
+
+    act(() => ReactDOM.createRoot(container).render(component));
+    expect(document.body.textContent).toContain("Jordan");
   });
 });
