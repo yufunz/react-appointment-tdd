289d2dca97718dabb7f2dd270bfd7569c4c27377
Yuhei <yuhei@X1C6-MAC.local>
2023-06-22 09:38:16 +1200

    Refactor AppointmentsDayView implementation and tests

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -29,6 +29,12 @@ describe("Appointment", () => {
 
 describe("AppointmentsDayView", () => {
   let container;
+  const today = new Date();
+  const twoAppointments = [
+    { startsAt: today.setHours(12, 0) },
+    { startsAt: today.setHours(13, 0) }
+  ];
+
   beforeEach(() => {
     container = document.createElement("div");
     document.body.replaceChildren(container);
@@ -48,13 +54,15 @@ describe("AppointmentsDayView", () => {
   });
 
   it("renders an li for each appointment", () => {
-    const today = new Date();
-    const twoAppointments = [
-      { startsAt: today.setHours(12, 0) },
-      { startsAt: today.setHours(13, 0) }
-    ];
     render(<AppointmentsDayView appointments={twoAppointments} />);
     const listChildren = document.querySelectorAll("ol > li");
     expect(listChildren).toHaveLength(2);
+  });
+
+  it("renders the time of each appointment", () => {
+    render(<AppointmentsDayView appointments={twoAppointments} />);
+    const listChildren = document.querySelectorAll("li");
+    expect(listChildren[0].textContent).toEqual("12:00");
+    expect(listChildren[1].textContent).toEqual("13:00");
   });
 });
