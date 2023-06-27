13f01412c5d542234eacd7c525c0079d9c51c7af
yufunz <yuhei@X1C6-MAC.local>
2023-06-27 20:53:42 +1200

    Use toBeInputFieldOfType matcher

--- test/domMatchers.js
+++ test/domMatchers.js
@@ -1,3 +1,4 @@
 import { toContainText } from "./machers/toContainText";
 import { toHaveClass } from "./machers/toHaveClass";
-expect.extend({ toContainText, toHaveClass });
+import { toBeInputFieldOfType } from "./machers/toBeInputFieldOfType";
+expect.extend({ toContainText, toHaveClass, toBeInputFieldOfType });
